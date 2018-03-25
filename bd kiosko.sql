4ce45ac0347123d711c9de1bbc3c14a7697150c0
nicolasahu <niko_jara_ahumada@live.cl>
2018-03-24 22:11:00 -0300

    bd kiosko modificacion

--- bd kiosko.sql
+++ bd kiosko.sql
@@ -22,19 +22,19 @@ foreign key (id_tipo) references tipo_producto(id)
 
 create table venta(
 id int not null auto_increment,
+id_producto int,
 cantidad int ,
-primary key (id)
+primary key (id),
+foreign key (id_producto) references producto(id)
 );
 
 create table boleta(
 id int not null auto_increment,
-id_producto int,
 id_venta int,
 fecha datetime,
 subtotal int,
 total int,
 primary key(id),
-foreign key (id_producto) references producto(id),
 foreign key (id_venta)references venta(id)
 );
 
