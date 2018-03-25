418d9a763aa5682c5c797d598e79bdede22f7a5c
nicolasahu <niko_jara_ahumada@live.cl>
2018-03-24 21:45:54 -0300

    bd kiosko

---
+++ bd kiosko.sql
@@ -0,0 +1,49 @@
+create database kiosko;
+
+use kiosko;
+
+create table tipo_producto(
+id int not null auto_increment,
+descripcion varchar(150),
+primary key (id)
+);
+
+
+create table producto(
+id int not null auto_increment,
+id_tipo int,
+nombreProducto varchar(150),
+precio int,
+stock int,
+primary key (id),
+foreign key (id_tipo) references tipo_producto(id)
+);
+
+
+create table venta(
+id int not null auto_increment,
+cantidad int ,
+primary key (id)
+);
+
+create table boleta(
+id int not null auto_increment,
+id_producto int,
+id_venta int,
+fecha datetime,
+subtotal int,
+total int,
+primary key(id),
+foreign key (id_producto) references producto(id),
+foreign key (id_venta)references venta(id)
+);
+
+select * from producto;
+select * from tipo_producto;
+select * from venta;
+select * from boleta;
+
+
+
+
+
